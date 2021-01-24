3d3a22774fc988dd6972d634292bfb24296f8fd4
ILMentor <42318339+ILMentor@users.noreply.github.com>
2021-01-24 16:23:01 +0100

    Загрузил весь проект осознанно.

---
+++ Second project/Second project/src/Controller.java
@@ -0,0 +1,9 @@
+/** Класс содержит все методы, которые должны быть у пульта*/
+public interface Controller {
+    void showInfo();
+    void pressPowerButton();
+    void pressSoundUpButton();
+    void pressSoundDownButton();
+    void pressNextChannelButton();
+    void pressPreviousChannelButton();
+}
