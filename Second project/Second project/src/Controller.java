c6c54ce4f387b4a163aaafcbf5956c44e5f82986
ILMentor <dekstroer8202@gmail.com>
2021-01-25 11:30:08 +0100

    Add project, idk what`s going on

--- Second project/Second project/src/Controller.java
+++
@@ -1,9 +0,0 @@
-/** Класс содержит все методы, которые должны быть у пульта*/
-public interface Controller {
-    void showInfo();
-    void pressPowerButton();
-    void pressSoundUpButton();
-    void pressSoundDownButton();
-    void pressNextChannelButton();
-    void pressPreviousChannelButton();
-}
