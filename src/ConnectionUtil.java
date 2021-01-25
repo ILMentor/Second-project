a0beebbc221b8a8e99d881125267e06ae9766614
ILMentor <dekstroer8202@gmail.com>
2021-01-25 11:35:34 +0100

    Add project, idk what`s going on

---
+++ src/ConnectionUtil.java
@@ -0,0 +1,63 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionUtil {
+    Statement statement;
+    Connection connection;
+    public Connection getConnection(){
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/store?serverTimezone=Europe/Moscow",
+                    "root", "Bujhm18Tdf13");
+            System.out.println("Connection successful.");
+        } catch (SQLException e) {
+            System.out.println("Connection failed.");
+            System.out.println(e);
+        }
+        return connection;
+    }
+
+    public void createStatement(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            statement = getConnection().createStatement();
+            System.out.println("Statement created.");
+        }
+        catch (Exception ex) {
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        }
+    }
+
+    public void updateSound() {
+        try {
+            System.out.printf("Updated %d rows\n",
+                    statement.executeUpdate("update tvconfig set Sound = Sound + 1 where TV = 'TV'"));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("Connection closed.");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        connection = null;
+    }
+
+    public void checkConnection() {
+        try {
+            System.out.println("Connection closed: " + statement.isClosed());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
