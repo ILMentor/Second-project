39571ea25dfede436666e06d67b798fd41b26bbf
ILMentor <dekstroer8202@gmail.com>
2021-01-28 20:18:46 +0100

    Add step two: create table in database

--- src/SQLiteJDBC.java
+++ src/SQLiteJDBC.java
@@ -1,15 +1,28 @@
 import java.sql.*;
 public class SQLiteJDBC {
     public static void main(){
-        Connection connection = null;
-
+        Connection connection;
+        Statement statement;
         try{
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection("jdbc:sqlite:test.db");
+            System.out.println("Opened database successfully");
+
+            statement = connection.createStatement();
+            String sql = "CREATE TABLE COMPANY " +
+                    "(ID INT PRIMARY KEY NOT NULL," +
+                    " NAME           TEXT NOT NULL," +
+                    " AGE            INT NOT NULL," +
+                    " ADDRESS        CHAR(50)," +
+                    " SALARY         REAL)";
+
+            statement.executeUpdate(sql);
+            statement.close();
+            connection.close();
+            System.out.println("Table create successfully.");
         } catch (Exception exception){
             System.err.println(exception.getClass().getName() + ": " + exception.getMessage());
             System.exit(0);
         }
-        System.out.println("Opened database successfully");
     }
 }
