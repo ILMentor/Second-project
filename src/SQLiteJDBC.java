43d110653fc94c68cb5403cf2e8d993dc2bb7a91
ILMentor <dekstroer8202@gmail.com>
2021-01-28 20:24:22 +0100

    Little change. Add todo in file

--- src/SQLiteJDBC.java
+++ src/SQLiteJDBC.java
@@ -15,11 +15,12 @@ public class SQLiteJDBC {
                     " AGE            INT NOT NULL," +
                     " ADDRESS        CHAR(50)," +
                     " SALARY         REAL)";
-
             statement.executeUpdate(sql);
             statement.close();
             connection.close();
             System.out.println("Table create successfully.");
+            // todo Реализовать базу данных. Добавить комманды.
+            // todo Сохранять результаты работы программы в базу данных
         } catch (Exception exception){
             System.err.println(exception.getClass().getName() + ": " + exception.getMessage());
             System.exit(0);
