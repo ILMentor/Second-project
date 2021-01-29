69b6d6ca6f3a3d08ad24f2488023fb0ba201053b
ILMentor <dekstroer8202@gmail.com>
2021-01-29 12:30:59 +0100

    Finish SQLiteJDBC class. Add methods to work with.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        SQLiteJDBC.main();
+        SQLiteJDBC sqLiteJDBC = new SQLiteJDBC();
+        sqLiteJDBC.getConnection();
+        sqLiteJDBC.createStatement();
+        sqLiteJDBC.closeConnection();
+        sqLiteJDBC.closeStatement();
     }
 }
--- src/SQLiteJDBC.java
+++ src/SQLiteJDBC.java
@@ -1,29 +1,80 @@
 import java.sql.*;
 public class SQLiteJDBC {
-    public static void main(){
-        Connection connection;
-        Statement statement;
-        try{
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:test.db");
-            System.out.println("Opened database successfully");
+    Connection connection;
+    Statement statement;
 
+    // todo Реализовать базу данных. Добавить комманды.
+    // todo Сохранять результаты работы программы в базу данных
+    // todo Добавить документацию к классу
+
+    public void getConnection(){
+        try{Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:test.db");
+            System.out.println("Connection successful.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Connection failed.");
+        }
+    }
+
+    public void createStatement(){
+        try {
             statement = connection.createStatement();
-            String sql = "CREATE TABLE COMPANY " +
-                    "(ID INT PRIMARY KEY NOT NULL," +
-                    " NAME           TEXT NOT NULL," +
-                    " AGE            INT NOT NULL," +
-                    " ADDRESS        CHAR(50)," +
-                    " SALARY         REAL)";
+            System.out.println("Statement created.");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            System.out.println("Statement create fell.");
+        }
+    }
+
+    public void createTable(String sql){
+        try{
+
             statement.executeUpdate(sql);
-            statement.close();
-            connection.close();
             System.out.println("Table create successfully.");
-            // todo Реализовать базу данных. Добавить комманды.
-            // todo Сохранять результаты работы программы в базу данных
         } catch (Exception exception){
             System.err.println(exception.getClass().getName() + ": " + exception.getMessage());
             System.exit(0);
         }
     }
+
+    public void executeUpdate(String sql){
+        try {
+            statement.executeUpdate(sql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void closeConnection(){
+        try {
+            connection.close();
+            System.out.println("Connection closed.");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            System.out.println("Connection close fell.");
+        }
+    }
+
+    public void closeStatement(){
+        try {
+            statement.close();
+            System.out.println("Statement closed.");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            System.out.println("Statement close fell.");
+        }
+    }
+
+    public void checkConnectionClose(){
+        try {
+            if (connection.isClosed())
+                System.out.println("Connection is closed.");
+            else
+                System.out.println("Connection is opened.");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
 }
