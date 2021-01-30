43e45f01616733ba1bf928f942ba73794656e807
ILMentor <dekstroer8202@gmail.com>
2021-01-30 22:23:21 +0100

    Добавил метод для вызова всех данных из базы данных.
    Научился вызывать данные из базы данных. Очень много нового узнал о документации.

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,22 @@ public class Main {
         SQLiteJDBC sqLiteJDBC = new SQLiteJDBC();
         sqLiteJDBC.getConnection();
         sqLiteJDBC.createStatement();
-        // todo Научиться вызывать данные из базы данных
-        // todo Исправить ошибки
         try {
-            ResultSet rs = sqLiteJDBC.statement.executeQuery("SELECT * FROM COMPANY;");
-            System.out.println("ID = " + rs.getInt("ID"));
-            System.out.println("NAME = " + rs.getString("NAME"));
-            System.out.println("AGE = " + rs.getInt("AGE"));
-            System.out.println("ADDRESS = " + rs.getString("ADDRESS"));
-            System.out.println("SALARY = " + rs.getInt("SALARY"));
-            System.out.println("NAME = " + rs.getString("NAME"));
-            System.out.println("NAME = " + rs.getString("NAME"));
+            ResultSet rs = sqLiteJDBC.statement.executeQuery( "SELECT * FROM COMPANY;" );
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String  name = rs.getString("name");
+                int age  = rs.getInt("age");
+                String  address = rs.getString("address");
+                float salary = rs.getFloat("salary");
+
+                System.out.println( "ID = " + id );
+                System.out.println( "NAME = " + name );
+                System.out.println( "AGE = " + age );
+                System.out.println( "ADDRESS = " + address );
+                System.out.println( "SALARY = " + salary );
+                System.out.println();
+            }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
