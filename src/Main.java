8f26fd73609980630f3681f777d482fae5a89493
ILMentor <dekstroer8202@gmail.com>
2021-01-31 21:13:21 +0100

    Первая попытка.
    Пробовал реализовать запись данных в базу данных.

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,31 @@ public class Main {
 
     public static void main(String[] args) {
         SQLiteJDBC sqLiteJDBC = new SQLiteJDBC();
+        Controller controller = new ControllerRealization(new TV());
         sqLiteJDBC.getConnection();
         sqLiteJDBC.createStatement();
+        controller.showInfo();
         try {
-            ResultSet rs = sqLiteJDBC.statement.executeQuery( "SELECT * FROM TV;" );
+            ResultSet rs = sqLiteJDBC.statement.executeQuery( "SELECT * FROM TV where ID = 1;" );
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                boolean power = rs.getBoolean("power");
+                int channel  = rs.getInt("channel");
+                int sound = rs.getInt("sound");
+
+                System.out.println( "ID = " + id );
+                System.out.println( "power = " + power );
+                System.out.println( "channel = " + channel );
+                System.out.println( "sound = " + sound );
+                System.out.println();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        controller.pressPowerButton();
+        controller.showInfo();
+        try {
+            ResultSet rs = sqLiteJDBC.statement.executeQuery( "SELECT * FROM TV where ID = 1;" );
             while (rs.next()) {
                 int id = rs.getInt("id");
                 boolean power = rs.getBoolean("power");
